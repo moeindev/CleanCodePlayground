343d987cf301ea950204bb1bc3f329eb0d68ed1d
Mohammad Moein Abdi <moeinabdi77@gmail.com>
2021-08-24 20:12:03 +0430

    initial commit 🔥

---
+++ src/ir/moeindeveloper/cleanCodePlayground/CleanCodePlayground.java
@@ -0,0 +1,9 @@
+package ir.moeindeveloper.cleanCodePlayground;
+
+public class CleanCodePlayground {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+}
